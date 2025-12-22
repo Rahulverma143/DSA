40809d9b90f6a26851086d7906ebf65743af16cf
Rahulverma143 <rahulvearma17@gmail.com>
2025-12-22 09:29:17 +0530

    Add Two string

---
+++ src/leetcode/AddTwoString.java
@@ -0,0 +1,16 @@
+//package leetcode;
+//
+//public class AddTwoString {
+//public static void main(String[] args) {
+//	String s1 = "1234";
+//	String s2 = "1234";
+//	 
+//	int n1 = Integer.parseInt(s1);
+//	int n2 = Integer.parseInt(s2);
+//	int sum = n1 + n2;
+//	System.out.println(sum);
+//	
+//  }
+//}
+	
+
