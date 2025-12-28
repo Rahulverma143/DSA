d119d0972de6f3610869fa1ce08058a3e0e1f79d
Rahulverma143 <rahulvearma17@gmail.com>
2025-12-29 00:17:16 +0530

    missing number

--- src/leetcode/MissingNumber_268.java
+++ src/leetcode/MissingNumber_268.java
@@ -1,11 +1,11 @@
 package leetcode;
 
-// 1. Missing Number
+// 1. Missing Number 
 public class MissingNumber_268 {
 
     public static void main(String[] args) {
 
-        int[] nums = {3, 0, 1};  
+        int[] nums = {3, 0, 1, 5, 4};  
 
         int n = nums.length;
         int total = n * (n + 1) / 2;
@@ -16,6 +16,6 @@ public class MissingNumber_268 {
         }
 
         int missing = total - sum;
-        System.out.println(missing);
+        System.out.println("Missing Number is : " + missing);
     }
 }
